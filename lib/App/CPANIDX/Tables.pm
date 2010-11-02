7422d50b8d40dd2ae30090b984fec8c169187c5f
Chris Williams <chris@bingosnet.co.uk>
2010-11-02 12:43:05 +0000

    Import of BINGOS/App-CPANIDX-0.26 from CPAN.
    gitpan-cpan-distribution: App-CPANIDX
    gitpan-cpan-version:      0.26
    gitpan-cpan-path:         BINGOS/App-CPANIDX-0.26.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/App/CPANIDX/Tables.pm
+++ lib/App/CPANIDX/Tables.pm
@@ -5,7 +5,7 @@ use warnings;
 use Module::CoreList::DBSchema;
 use vars qw[$VERSION];
 
-$VERSION = '0.24';
+$VERSION = '0.26';
 
 my $mcdbs = Module::CoreList::DBSchema->new();
 my %cl_tables = $mcdbs->tables();
@@ -14,7 +14,7 @@ my $tables = {
    mods => [
       'mod_name VARCHAR(300) NOT NULL',
       'dist_name VARCHAR(190) NOT NULL',
-      'dist_vers VARCHAR(20)',
+      'dist_vers VARCHAR(50)',
       'cpan_id VARCHAR(20) NOT NULL',
       'mod_vers VARCHAR(30)',
     ],
@@ -22,7 +22,7 @@ my $tables = {
       'dist_name VARCHAR(190) NOT NULL',
       'cpan_id VARCHAR(20) NOT NULL',
       'dist_file VARCHAR(400) NOT NULL',
-      'dist_vers VARCHAR(20)',
+      'dist_vers VARCHAR(50)',
     ],
    auths => [
       'cpan_id VARCHAR(20) NOT NULL',
